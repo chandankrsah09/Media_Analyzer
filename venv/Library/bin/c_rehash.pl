c55d33feb9d85b3ea6a5c57410e82499170cba4f
Chandan <dbgchandan441@gmail.com>
2025-01-06 21:24:44 +0530

    final

--- venv/Library/bin/c_rehash.pl
+++ venv/Library/bin/c_rehash.pl
@@ -13,7 +13,7 @@
 # and add symbolic links to their hash values.
 
 my $dir = "C:\\Program Files\\Common Files\\ssl";
-my $prefix = "C:/Users/dbgch/OneDrive/Desktop/AI/SMPA/insightflow/venv\\Library";
+my $prefix = "C:/Users/dbgch/OneDrive/Desktop/AI/SMPA/MediaAnalyzer/venv\\Library";
 
 my $errorcount = 0;
 my $openssl = $ENV{OPENSSL} || "openssl";
